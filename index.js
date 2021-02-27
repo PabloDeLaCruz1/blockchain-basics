535c11881646b7dfc135d8f55c08a04923b6c6d1
Pablo De La Cruz <pdelac@ramapo.edu>
2021-02-27 12:06:27 -0500

    ready to test

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 import sha256 from 'crypto-js/sha256';
+
+let generateHash = (obj) => sha256(JSON.stringify(object));
+
+const data1 = "Blockchain Rock!";
+const dataObject = {
+    id: 1,
+    body: "With Object Works too",
+    time: new Date().getTime().toString().slice(0, -3)
+};
+
+console.log(`SHA256 Hash: ${generateHash(data1)}`);
+console.log("************************************");
+console.log(`SHA256 Hash: ${generateHash(dataObject)}`);
