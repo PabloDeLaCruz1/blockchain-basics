8a5848db5e0a7bc4c48e0db5cfa601f66e50cab7
Pablo De La Cruz <pdelac@ramapo.edu>
2021-02-27 22:38:47 -0500

    generating hash in block

---
+++ App.js
@@ -0,0 +1,15 @@
+const BlockClass = require('./block.js');
+
+/**
+ * Creating a block object
+ */
+const block = new BlockClass.Block("Test Block");
+
+// Generating the block hash
+block.generateHash().then((result) => {
+    console.log(result);
+    console.log(`Block Hash-----------: ${result.hash}`);
+    console.log(`Block: ${JSON.stringify(result)}`);
+}).catch((error) => {
+    console.log(error)
+});
--- Block.js
+++ Block.js
@@ -1,11 +1,23 @@
 import sha256 from 'crypto-js/sha256';
+// import Promise from 'bluebird';
 
 let Block = class {
-    constructor(previousHash, timestamp) {
-        this.previousHash = previousHash;
-        this.timestamp = timestamp
+    constructor(data) {
+        this.id = 0;
+        this.nonce = 144444;
+        this.body = data;
+        this.hash = "";
     }
-    generateHash = (obj) => {
-        this.hash = sha256(JSON.stringify(obj));
+    generateHash() {
+        let self = this;
+        return new Promise((resolve, reject) => {
+            self.hash = sha256(JSON.stringify(self)).toString()
+            if (self.hash != "") {
+                resolve(self);
+            } else {
+                reject(Error("It broke"));
+            }
+        })
     }
 }
+module.exports.Block = Block;
